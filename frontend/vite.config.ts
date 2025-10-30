30e4741de8a540cb0beb2bd8c64c888ac1a2a8b4
Alexander-Coding <71774264+Alexander-Coding@users.noreply.github.com>
2025-10-30 10:35:59 +0700

    Настройка alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,25 @@
-import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
+
+import { defineConfig } from 'vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+    plugins: [
+        react({
+            babel: {
+                plugins: [['babel-plugin-react-compiler']],
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            "@features": path.resolve(__dirname, "src/features"),
+            "@layouts":  path.resolve(__dirname, "src/layouts"),
+            "@pages":    path.resolve(__dirname, "src/pages"),
+            "@shared":   path.resolve(__dirname, "src/shared"),
+            "@root":     path.resolve(__dirname, "src"),
+        }
+    }
 })
